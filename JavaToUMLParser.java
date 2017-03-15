7e1f1af2afae88475b79a7258a9d60239fcf131b
karandidwani <karan.didwani@sjsu.edu>
2017-03-14 19:37:17 -0700

    Add files via upload

---
+++ JavaToUMLParser.java
@@ -0,0 +1,9 @@
+public Class JavaToUMLParser{
+ public static void main(String args[]){
+ 
+ CompilationUnit c = new CompilationUnit();
+ FileInputStream f = new FileInputStream("A.java");
+ 
+ 
+ }
+}
