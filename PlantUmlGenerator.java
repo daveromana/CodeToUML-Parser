ac4c4f65daba5d044e884d7576fec3c25db0c14d
karandidwani <karan.didwani@sjsu.edu>
2017-03-30 21:45:45 -0700

    Update PlantUmlGenerator.java

--- PlantUmlGenerator.java
+++ PlantUmlGenerator.java
@@ -7,16 +7,19 @@ import net.sourceforge.plantuml.GeneratedImage;
 import net.sourceforge.plantuml.SourceFileReader;
 
 public class PlantUmlGenerator {
+	
 	public void createUML(String fName) {
 		File src = new File(fName);
 		SourceFileReader read;
 		List<GeneratedImage> list;
+		
 		try {
 			read = new SourceFileReader(src);
 			list = read.getGeneratedImages();
 			File pngImage = list.get(0).getPngFile();
 
 		} catch (IOException e) {
+			
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
