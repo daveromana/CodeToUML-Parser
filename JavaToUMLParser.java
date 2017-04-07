4768e22d779422b3fd276879b0e8c10ac420c580
karandidwani <karan.didwani@sjsu.edu>
2017-04-06 17:17:43 -0700

    Delete JavaToUMLParser.java

--- JavaToUMLParser.java
+++
@@ -1,9 +0,0 @@
-public Class JavaToUMLParser{
- public static void main(String args[]){
- 
- CompilationUnit c = new CompilationUnit();
- FileInputStream f = new FileInputStream("A.java");
- 
- 
- }
-}
