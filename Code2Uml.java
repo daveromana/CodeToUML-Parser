d3ebb14be882db03cc0107cbcdec445212c4f591
karandidwani <karan.didwani@sjsu.edu>
2017-04-14 16:37:20 -0700

    Update Code2Uml.java

--- Code2Uml.java
+++ Code2Uml.java
@@ -1,14 +1,17 @@
 
 import japa.parser.JavaParser;
+import japa.parser.ParseException;
 import japa.parser.ast.CompilationUnit;
 import japa.parser.ast.body.ClassOrInterfaceDeclaration;
 import japa.parser.ast.body.FieldDeclaration;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.regex.Pattern;
 
 public class Code2Uml {
 
@@ -19,7 +22,7 @@ public class Code2Uml {
     static ArrayList<String> repAssociation = new ArrayList<String>();
     static ArrayList<String> finalOp = new ArrayList<>();
     static ArrayList<String> umlGeneratorIp = new ArrayList<>();
-    
+
     public static void main(String[] args) throws IOException {
 
         FileInputStream finStream = null;
@@ -61,25 +64,27 @@ public class Code2Uml {
                     createUMLInput();
 
                     //System.out.println(cu+"\n\n");
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } catch (ParseException | FileNotFoundException e) {
                 } finally {
                     try {
                         finStream.close();
                     } catch (IOException e) {
-                        e.printStackTrace();
                     }
                 }
             }
         }
         umlGeneratorIp.add("@enduml");
-        
-        System.out.println();
+
+        /*System.out.println();
         umlGeneratorIp.forEach((s) -> {
             System.out.print(s);
-        });
-        
-        new ClassDiagramGenerator().createClassDiagram(umlGeneratorIp.toString());
+        });*/
+        String umlGeneratorIpStr = umlGeneratorIp.toString().replaceAll(",", " ").replaceFirst(Pattern.quote("["), " ");
+        umlGeneratorIpStr = umlGeneratorIpStr.substring(0, umlGeneratorIpStr.length() - 1);
+        System.out.println(umlGeneratorIpStr);
+
+        // final call to plantUmlGenerator
+        new ClassDiagramGenerator().createClassDiagram(umlGeneratorIpStr);
     }
 
     private static void createUMLInput() {
@@ -105,13 +110,11 @@ public class Code2Uml {
 
         System.out.println();
         finalOp.forEach((s) -> {
-        umlGeneratorIp.add(s);
-            System.out.print(s);
+            umlGeneratorIp.add(s);
+            //System.out.print(s);
         });
-        
 
-        System.out.println();
-
+        //System.out.println();
         finalOp.clear();
         allVariables.clear();
         isAssosiatedTo.clear();
@@ -125,7 +128,7 @@ public class Code2Uml {
 
             String classVariables;
             String variableWdBracs = fd.getVariables().toString();
-            ArrayList<String> types = new ArrayList<String>();
+            ArrayList<String> types = new ArrayList<>();
             types.add(fd.getType().toString());
 
             if (fd.getModifiers() == 2) {
