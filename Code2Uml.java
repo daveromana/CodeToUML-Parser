fa7e29a0f8371b6873477741b6af7053d6cd7cf5
karandidwani <karan.didwani@sjsu.edu>
2017-04-18 10:35:24 -0700

    Update Code2Uml.java

--- Code2Uml.java
+++ Code2Uml.java
@@ -1,4 +1,3 @@
-
 import japa.parser.JavaParser;
 import japa.parser.ParseException;
 import japa.parser.ast.CompilationUnit;
@@ -14,33 +13,32 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Code2Uml {
-
+    
     static ArrayList<String> javaFiles = new ArrayList();
     static String[] allFiles;
     static boolean isInterface;
-
+    
     //variables used in fetching all the fields
     static ArrayList<String> allVariables = new ArrayList<String>();
     static ArrayList<String> isAssosiatedTo = new ArrayList<String>();
     static ArrayList<String> repAssociation = new ArrayList<String>();
     static ArrayList<String> finalOp = new ArrayList<>();
     static ArrayList<String> umlGeneratorIp = new ArrayList<>();
-
+    static ArrayList<String> varNames = new ArrayList<>();
+    
     //variables used in finding all the methods
     static ArrayList<String> allMethods = new ArrayList<String>();
-
+    
     //variable to get interfaces and child classes
     static ArrayList<String> allInterfaceNClasses = new ArrayList<String>();
-
+    
     public static void main(String[] args) throws IOException {
-
         FileInputStream finStream = null;
         CompilationUnit cu;
         String inputDirName = "C:\\Users\\Karan\\Downloads\\202 downloads\\cmpe202-master\\cmpe202-master\\umlparser\\uml-parser-test-3";
         File inputFile = new File(inputDirName);
         File[] inputFileList = inputFile.listFiles();
         String classNames;
-
         //fetch all the class names in a file
         for (File f : inputFileList) {
             classNames = f.getName();
@@ -51,7 +49,6 @@ public class Code2Uml {
         }
 
         umlGeneratorIp.add("@startuml \n");
-
         for (File f2 : inputFileList) {
             classNames = f2.getName();
             allFiles = classNames.split("\\.");
@@ -61,23 +58,17 @@ public class Code2Uml {
                 try {
                     finStream = new FileInputStream(fileName);
                     cu = JavaParser.parse(finStream);
-
                     isInterface = cu.toString().contains(" interface ");
-
                     //calling methods to find variables
                     GetVariables getVar = new GetVariables();
                     getVar.visit(cu, null);
-
                     //calling methods to get all methods in the test cases
                     GetMethods getMet = new GetMethods();
                     getMet.visit(cu, null);
-
                     //calling methods to find what classes being extended or interfaces being implemented
                     GetClassesOrInterfaces getCls = new GetClassesOrInterfaces();
                     getCls.visit(cu, 0);
-
                     createUMLInput();
-
                     //System.out.println(cu+"\n\n");
                 } catch (ParseException | FileNotFoundException e) {
                 } finally {
@@ -98,16 +89,13 @@ public class Code2Uml {
             } else {
                 umlGeneratorIpStr = umlGeneratorIpStr + s;
             }
-
         }
         System.out.println(umlGeneratorIpStr);
-
         // final call to plantUmlGenerator
         new ClassDiagramGenerator().createClassDiagram(umlGeneratorIpStr);
     }
-
+    
     private static void createUMLInput() {
-
         if (isInterface) {
             finalOp.add("interface " + allFiles[0]);
         } else {
@@ -118,45 +106,38 @@ public class Code2Uml {
             finalOp.add(var);
             finalOp.add("\n");
         });
-
         Iterator<String> iter = finalOp.iterator();
-
         while (iter.hasNext()) {
             String b = iter.next();
-
             repAssociation.stream().filter((a) -> (a.equals(b))).forEachOrdered((_item) -> {
                 iter.remove();
             });
         }
-
         for (String a : allMethods) {
             finalOp.add(a);
         }
-
         finalOp.add("\n}\n\n");
-
         finalOp.forEach((s) -> {
             umlGeneratorIp.add(s);
             //System.out.print(s);
         });
-
         finalOp.clear();
         allVariables.clear();
         isAssosiatedTo.clear();
         allMethods.clear();
+        varNames.clear();
     }
-
+    
     //Class for fetching variables in the test classes
     private static class GetVariables extends VoidVisitorAdapter {
-
         @Override
         public void visit(FieldDeclaration fd, Object obj) {
-
             String classVariables;
             String variableWdBracs = fd.getVariables().toString();
             ArrayList<String> types = new ArrayList<>();
             types.add(fd.getType().toString());
-
+            varNames.add(fd.getVariables().toString().replaceAll("\\[", "").replaceAll("]", ""));
+            
             if (fd.getModifiers() == 2) {
                 classVariables = "- " + variableWdBracs.substring(1, variableWdBracs.length() - 1) + " : " + fd.getType();
                 allVariables.add(classVariables);
@@ -164,11 +145,9 @@ public class Code2Uml {
                 classVariables = "+ " + variableWdBracs.substring(1, variableWdBracs.length() - 1) + " : " + fd.getType();
                 allVariables.add(classVariables);
             }
-
             //Association Logic goes here..
             for (String s : types) {
                 for (String className : javaFiles) {
-
                     if (isAssosiatedTo.contains(s)) {
                         break;
                     }
@@ -189,17 +168,15 @@ public class Code2Uml {
             }
         }
     }
-
+    
     //Class for extracting all methods in the test codes
     private static class GetMethods extends VoidVisitorAdapter<Object> {
-
         @Override
         public void visit(MethodDeclaration md, Object o) {
-
             String methods;
-
+            
+            
             if (md.getParameters() == null) {
-
                 if (md.getModifiers() == 1) {
                     methods = "+ " + md.getName() + "() : " + md.getType();
                     allMethods.add(methods);
@@ -210,15 +187,11 @@ public class Code2Uml {
                     allMethods.add("\n");
                 }
             } else {
-
                 String[] param = md.getParameters().toString().replace("]", "").replace("[", "").split(",");
                 String[] singleParam;
-
                 int numOfPrm = md.getParameters().size();
-
                 if (numOfPrm == 1) {
                     singleParam = param[0].split(" ");
-
                     for (String a : javaFiles) {
                         if (a.equals(singleParam[0])) {
                             //System.out.println("code running "+parName[0]+"..>"+a);
@@ -226,7 +199,6 @@ public class Code2Uml {
                             finalOp.add("\n");
                         }
                     }
-
                     if (md.getModifiers() == 1) {
                         methods = "+ " + md.getName() + "( " + singleParam[1] + ": " + singleParam[0] + ") : " + md.getType();
                         allMethods.add(methods);
@@ -236,7 +208,6 @@ public class Code2Uml {
                         allMethods.add(methods);
                         allMethods.add("\n");
                     }
-
                 } else {
                     if (md.getModifiers() == 1) {
                         methods = "+ " + md.getName() + "(";
@@ -259,32 +230,26 @@ public class Code2Uml {
                         methods = parName[1] + ":" + parName[0];
                         allMethods.add(methods);
                         allMethods.add(",");
-                        
                     }
-
                     allMethods.add("):");
                     allMethods.add(md.getType().toString());
                 }
-
                 if (numOfPrm > 1) {
                     allMethods.remove(allMethods.lastIndexOf(","));
                 }
             }
         }
     }
-
+    
     //Class for Extracting all the classes
     private static class GetClassesOrInterfaces extends VoidVisitorAdapter {
-
         @Override
         public void visit(ClassOrInterfaceDeclaration cid, Object obj) {
             ArrayList<String> classesExtended = new ArrayList<>();
-
             if (cid.getExtends() != null) {
                 String c1 = allFiles[0] + " --|> " + cid.getExtends().toString().replace("[", "").replace("]", "");
                 finalOp.add(c1 + "\n");
             }
-
             if (cid.getImplements() != null) {
                 int num = cid.getImplements().size();
                 if (num == 1) {
@@ -298,7 +263,6 @@ public class Code2Uml {
                     }
                 }
             }
-
         }
     }
 }
